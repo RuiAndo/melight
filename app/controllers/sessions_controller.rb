50e846be437aaf3459f4b2b700710069573565ab
RuiAndo <onlyisnotlonelyrui@yahoo.co.jp>
2018-10-23 22:56:12 +0900

    add user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:success] = "ログインしました"
+      redirect_to :root
+    else
+      flash[:error] = "ログインに失敗しました"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    @current_user = nil
+    flash[:success] = "ログアウトしました"
+    redirect_to :root
+  end
+
+end
